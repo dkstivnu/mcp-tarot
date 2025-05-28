a39b18cb5262e2654002c542d2a0ea2c5a494377
stivn <wilstivenortega@gmail.com>
2025-05-28 16:33:55 -0500

    creado el metodo leerSigno()

--- src/main/java/com/tarot/model/Zodiaco.java
+++ src/main/java/com/tarot/model/Zodiaco.java
@@ -1,24 +1,25 @@
 package com.tarot.model;
 
 import java.time.LocalDate;
+import java.time.MonthDay;
 
 public enum Zodiaco {
 
-    Aries("Aries"),
-    Tauro("Tauro"),
-    Geminis("Géminis"),
-    Cancer("Cáncer"),
-    Leo("Leo"),
-    Libra("Libra"),
-    Escorpio("Escorpio"),
-    Sagitario("Sagitario"),
-    Capricornio("Capricornio"),
-    Acuario("Acuario"),
-    Piscis("Piscis");
+    Aries(Zodiaco.valueOf("Aries")),
+    Tauro(Zodiaco.valueOf("Tauro")),
+    Geminis(Zodiaco.valueOf("Géminis")),
+    Cancer(Zodiaco.valueOf("Cáncer")),
+    Leo(Zodiaco.valueOf("Leo")),
+    Libra(Zodiaco.valueOf("Libra")),
+    Escorpio(Zodiaco.valueOf("Escorpio")),
+    Sagitario(Zodiaco.valueOf("Sagitario")),
+    Capricornio(Zodiaco.valueOf("Capricornio")),
+    Acuario(Zodiaco.valueOf("Acuario")),
+    Piscis(Zodiaco.valueOf("Piscis"));
 
-    private String name;
+    private final Zodiaco name;
 
-    Zodiaco(String nombre) {
+    Zodiaco(Zodiaco nombre) {
         this.name = nombre;
     }
 
@@ -26,24 +27,71 @@ public enum Zodiaco {
         this.name = leerSigno(fechaNacimiento);
     }
 
-    public String getName() {
+    public Zodiaco getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     // Metodos propios
 
-    private static String leerSigno(LocalDate fechaNacimiento) {
-        LocalDate[] startFechas = new LocalDate[]{
+    public static Zodiaco leerSigno(LocalDate fechaNacimiento) throws RuntimeException {
+        MonthDay nacimientoMD = MonthDay.from(fechaNacimiento);
 
+        MonthDay[] startFechas = new MonthDay[]{
+                MonthDay.from(LocalDate.of(0, 3, 21)), // Aries
+                MonthDay.from(LocalDate.of(0, 4, 20)), // Tauro
+                MonthDay.from(LocalDate.of(0, 5, 21)), // Geminis
+                MonthDay.from(LocalDate.of(0, 6, 21)), // Cancer
+                MonthDay.from(LocalDate.of(0, 7, 23)), // Leo
+                MonthDay.from(LocalDate.of(0, 8, 23)), // Virgo
+                MonthDay.from(LocalDate.of(0, 9, 23)), // Libra
+                MonthDay.from(LocalDate.of(0, 10, 23)),// Escorpio
+                MonthDay.from(LocalDate.of(0, 11, 22)),// Sagitario
+                MonthDay.from(LocalDate.of(0, 12, 22)),// Capricornio
+                MonthDay.from(LocalDate.of(0, 1, 1)),  // Capricornio
+                MonthDay.from(LocalDate.of(0, 1, 20)), // Acuario
+                MonthDay.from(LocalDate.of(0, 2, 19)), // Piscis
         };
-        LocalDate[] endFechas = new LocalDate[]{
-
+        MonthDay[] endFechas = new MonthDay[]{
+                MonthDay.from(LocalDate.of(0, 4, 19)), // Aries
+                MonthDay.from(LocalDate.of(0, 5, 20)), // Tauro
+                MonthDay.from(LocalDate.of(0, 6, 20)), // Geminis
+                MonthDay.from(LocalDate.of(0, 7, 22)), // Cancer
+                MonthDay.from(LocalDate.of(0, 8, 22)), // Leo
+                MonthDay.from(LocalDate.of(0, 9, 22)), // Virgo
+                MonthDay.from(LocalDate.of(0, 10, 22)),// Libra
+                MonthDay.from(LocalDate.of(0, 11, 21)),// Escorpio
+                MonthDay.from(LocalDate.of(0, 12, 21)),// Sagitario
+                MonthDay.from(LocalDate.of(0, 12, 31)),// Capricornio
+                MonthDay.from(LocalDate.of(0, 1, 19)), // Capricornio
+                MonthDay.from(LocalDate.of(0, 2, 18)), // Acuario
+                MonthDay.from(LocalDate.of(0, 3, 20)), // Piscis
         };
 
-        return fechaNacimiento.toString();
+        Zodiaco[] signos = new Zodiaco[]{
+                Aries,
+                Tauro,
+                Geminis,
+                Cancer,
+                Leo,
+                Libra,
+                Escorpio,
+                Sagitario,
+                Capricornio,
+                Capricornio,
+                Acuario,
+                Piscis
+        };
+
+        int i;
+        for (i = 0; i <= 13; i++) {
+            if (nacimientoMD.equals(startFechas[i]) || nacimientoMD.equals(endFechas[i])) {
+                return signos[i];
+            }
+            if (nacimientoMD.isAfter(startFechas[i]) || nacimientoMD.isBefore(endFechas[i])) {
+                return signos[i];
+            }
+        }
+
+        throw new RuntimeException("No se ha podido leer el signo correctamente");
     }
 }
