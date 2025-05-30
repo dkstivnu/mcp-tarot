e890d0729bda20e78d43e9c28f9c3c9981b22240
stivn <wilstivenortega@gmail.com>
2025-05-30 04:17:23 -0500

    CREACION DEL JSON

--- src/main/java/com/tarot/tools/GenPrediccion.java
+++ src/main/java/com/tarot/tools/GenPrediccion.java
@@ -3,12 +3,14 @@ package com.tarot.tools;
 import com.jetbrains.exported.JBRApi;
 import io.modelcontextprotocol.server.McpServerFeatures;
 import io.modelcontextprotocol.spec.McpSchema;
+import io.modelcontextprotocol.spec.McpSchema.JsonSchema;
 
 @JBRApi.Service
 public class GenPrediccion {
 
-    McpSchema.JsonSchema schema = new McpSchema.JsonSchema("Fecha de nacimiento", ) {
-    });
+
+    McpSchema.JsonSchema schema = new McpSchema.JsonSchema();
+
     McpServerFeatures.SyncToolSpecification syncToolSpecification = new McpServerFeatures.SyncToolSpecification(
             new McpSchema.Tool("GenLectura",
                     "Genera la lectura del tarot a partir de la fecha de nacimiento",
