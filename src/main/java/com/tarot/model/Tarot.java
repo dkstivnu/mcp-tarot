db7116e614ee53802c2ea1e1e1f3f18c0f1bdc3e
stivn <wilstivenortega@gmail.com>
2025-05-29 21:36:10 -0500

    uso de LinkedList

--- src/main/java/com/tarot/model/Tarot.java
+++ src/main/java/com/tarot/model/Tarot.java
@@ -1,36 +1,41 @@
 package com.tarot.model;
 
-import java.util.ArrayList;
+
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class Tarot {
+
     //Atributos
-    private List<String> carta;
-    private List<String> significado;
+
+    private LinkedList<String> cartas;
+    private LinkedList<String> significado;
     private Random random;
 
     //Constructores
+
     public Tarot() {
-        this.carta = new ArrayList<>();
-        this.significado = new ArrayList<>();
+        this.cartas = new LinkedList<>();
+        this.significado = new LinkedList<>();
         this.random = new Random();
     }
-    //get y set
 
-    public List<String> getCarta() {
-        return carta;
+    //Metodos get y set
+
+    public List<String> getCartas() {
+        return cartas;
     }
 
-    public void setCarta(List<String> carta) {
-        this.carta = carta;
+    public void setCartas(LinkedList<String> cartas) {
+        this.cartas = cartas;
     }
 
     public List<String> getSignificado() {
         return significado;
     }
 
-    public void setSignificado(List<String> significado) {
+    public void setSignificado(LinkedList<String> significado) {
         this.significado = significado;
     }
 
@@ -44,102 +49,99 @@ public class Tarot {
 
     //Metodos propios
     public String seleccionarCartaAleatoria() {
-        return carta.get(random.nextInt(carta.size()));
+        return cartas.get(random.nextInt(cartas.size()));
     }
     public void inicializarCartas() {
 
         // Arcanos Mayores
-        carta.add("El Loco");
-        carta.add("El Mago");
-        carta.add("La Papisa");
-        carta.add("La Emperatriz");
-        carta.add("El Emperador");
-        carta.add("El Papa");
-        carta.add("Los Enamorados");
-        carta.add("El Carro");
-        carta.add("La Justicia");
-        carta.add("El Ermitaño");
-        carta.add("La Rueda de la Fortuna");
-        carta.add("La Fuerza");
-        carta.add("El Colgado");
-        carta.add("La Muerte");
-        carta.add("La Templanza");
-        carta.add("El Diablo");
-        carta.add("La Torre");
-        carta.add("La Estrella");
-        carta.add("La Luna");
-        carta.add("El Sol");
-        carta.add("El Juicio");
-        carta.add("El Mundo");
+
+        cartas.add("El Loco");
+        cartas.add("El Mago");
+        cartas.add("La Papisa");
+        cartas.add("La Emperatriz");
+        cartas.add("El Emperador");
+        cartas.add("El Papa");
+        cartas.add("Los Enamorados");
+        cartas.add("El Carro");
+        cartas.add("La Justicia");
+        cartas.add("El Ermitaño");
+        cartas.add("La Rueda de la Fortuna");
+        cartas.add("La Fuerza");
+        cartas.add("El Colgado");
+        cartas.add("La Muerte");
+        cartas.add("La Templanza");
+        cartas.add("El Diablo");
+        cartas.add("La Torre");
+        cartas.add("La Estrella");
+        cartas.add("La Luna");
+        cartas.add("El Sol");
+        cartas.add("El Juicio");
+        cartas.add("El Mundo");
 
         // Arcanos Menores - Copas
-        carta.add("As de Copas");
-        carta.add("Dos de Copas");
-        carta.add("Tres de Copas");
-        carta.add("Cuatro de Copas");
-        carta.add("Cinco de Copas");
-        carta.add("Seis de Copas");
-        carta.add("Siete de Copas");
-        carta.add("Ocho de Copas");
-        carta.add("Nueve de Copas");
-        carta.add("Diez de Copas");
-        carta.add("Sota de Copas");
-        carta.add("Caballo de Copas");
-        carta.add("Reina de Copas");
-        carta.add("Rey de Copas");
+        cartas.add("As de Copas");
+        cartas.add("Dos de Copas");
+        cartas.add("Tres de Copas");
+        cartas.add("Cuatro de Copas");
+        cartas.add("Cinco de Copas");
+        cartas.add("Seis de Copas");
+        cartas.add("Siete de Copas");
+        cartas.add("Ocho de Copas");
+        cartas.add("Nueve de Copas");
+        cartas.add("Diez de Copas");
+        cartas.add("Sota de Copas");
+        cartas.add("Caballo de Copas");
+        cartas.add("Reina de Copas");
+        cartas.add("Rey de Copas");
 
         // Arcanos Menores - Espadas
-        carta.add("As de Espadas");
-        carta.add("Dos de Espadas");
-        carta.add("Tres de Espadas");
-        carta.add("Cuatro de Espadas");
-        carta.add("Cinco de Espadas");
-        carta.add("Seis de Espadas");
-        carta.add("Siete de Espadas");
-        carta.add("Ocho de Espadas");
-        carta.add("Nueve de Espadas");
-        carta.add("Diez de Espadas");
-        carta.add("Sota de Espadas");
-        carta.add("Caballo de Espadas");
-        carta.add("Reina de Espadas");
-        carta.add("Rey de Espadas");
+        cartas.add("As de Espadas");
+        cartas.add("Dos de Espadas");
+        cartas.add("Tres de Espadas");
+        cartas.add("Cuatro de Espadas");
+        cartas.add("Cinco de Espadas");
+        cartas.add("Seis de Espadas");
+        cartas.add("Siete de Espadas");
+        cartas.add("Ocho de Espadas");
+        cartas.add("Nueve de Espadas");
+        cartas.add("Diez de Espadas");
+        cartas.add("Sota de Espadas");
+        cartas.add("Caballo de Espadas");
+        cartas.add("Reina de Espadas");
+        cartas.add("Rey de Espadas");
 
         // Arcanos Menores - Bastos
-        carta.add("As de Bastos");
-        carta.add("Dos de Bastos");
-        carta.add("Tres de Bastos");
-        carta.add("Cuatro de Bastos");
-        carta.add("Cinco de Bastos");
-        carta.add("Seis de Bastos");
-        carta.add("Siete de Bastos");
-        carta.add("Ocho de Bastos");
-        carta.add("Nueve de Bastos");
-        carta.add("Diez de Bastos");
-        carta.add("Sota de Bastos");
-        carta.add("Caballo de Bastos");
-        carta.add("Reina de Bastos");
-        carta.add("Rey de Bastos");
+        cartas.add("As de Bastos");
+        cartas.add("Dos de Bastos");
+        cartas.add("Tres de Bastos");
+        cartas.add("Cuatro de Bastos");
+        cartas.add("Cinco de Bastos");
+        cartas.add("Seis de Bastos");
+        cartas.add("Siete de Bastos");
+        cartas.add("Ocho de Bastos");
+        cartas.add("Nueve de Bastos");
+        cartas.add("Diez de Bastos");
+        cartas.add("Sota de Bastos");
+        cartas.add("Caballo de Bastos");
+        cartas.add("Reina de Bastos");
+        cartas.add("Rey de Bastos");
 
         // Arcanos Menores - Oros
-        carta.add("As de Oros");
-        carta.add("Dos de Oros");
-        carta.add("Tres de Oros");
-        carta.add("Cuatro de Oros");
-        carta.add("Cinco de Oros");
-        carta.add("Seis de Oros");
-        carta.add("Siete de Oros");
-        carta.add("Ocho de Oros");
-        carta.add("Nueve de Oros");
-        carta.add("Diez de Oros");
-        carta.add("Sota de Oros");
-        carta.add("Caballo de Oros");
-        carta.add("Reina de Oros");
-        carta.add("Rey de Oros");
+        cartas.add("As de Oros");
+        cartas.add("Dos de Oros");
+        cartas.add("Tres de Oros");
+        cartas.add("Cuatro de Oros");
+        cartas.add("Cinco de Oros");
+        cartas.add("Seis de Oros");
+        cartas.add("Siete de Oros");
+        cartas.add("Ocho de Oros");
+        cartas.add("Nueve de Oros");
+        cartas.add("Diez de Oros");
+        cartas.add("Sota de Oros");
+        cartas.add("Caballo de Oros");
+        cartas.add("Reina de Oros");
+        cartas.add("Rey de Oros");
 
     }
-
-
-
-
 
 }
